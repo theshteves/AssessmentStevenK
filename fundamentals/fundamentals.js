0b706c35ced3f141138d82999d244f3b19f7ef4c
theshteves <stevenkneiser@gmail.com>
2022-01-25 19:21:03 -0500

    Answered fundamentals/minMax

--- fundamentals/fundamentals.js
+++ fundamentals/fundamentals.js
@@ -19,8 +19,12 @@
     minMax([1]) ➞ [1, 1]
 */
 
-function minMax (values) {
+function minMax(values) {
+  // Assumption: Since not all values in the array are known to be numbers & I don't have the time to be comprehensive, I took extra precaution in the face of ambiguous constraints to only accept numeric types (to avoid booleans implicitly casting to numbers & save me the time of enumerating tests for unpacking nested containers, sanitizing malicious strings, etc. ...although a simple RegEx might do).  This filter prevents compliant strings from getting implicitly cast, as well as booleans (e.g. true -> 1). Floats (e.g. 3.14159), exponentials (e.g. 2.048e3), negatives, & number-resolving expressions (e.g. Math.E) are supported.
+  values = values.filter(value => typeof(value) === 'number')
 
+  return [Math.min(...values), Math.max(...values)]
+  // While this optimizes for readability & developer time cost, I still managed to needlessly sacrifice time efficiency & nobody likes "silent failures" (I could at least detect them & leave a console.warn). I have a frontend & backend to do on top of other interviews so no testing suite today.
 }
 
 
