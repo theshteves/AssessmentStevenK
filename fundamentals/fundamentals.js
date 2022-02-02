efb7941ceb887bb9e680e0b40d41f4090ea8db42
theshteves <stevenkneiser@gmail.com>
2022-02-02 12:52:23 -0500

    On second pass, polished fundamentals solution

--- fundamentals/fundamentals.js
+++ fundamentals/fundamentals.js
@@ -20,13 +20,17 @@
 */
 
 function minMax(values) {
-  // Assumption: Since not all values in the array are known to be numbers & I don't have the time to be comprehensive, I took extra precaution in the face of ambiguous constraints to only accept numeric types (to avoid booleans implicitly casting to numbers & save me the time of enumerating tests for unpacking nested containers, sanitizing malicious strings, etc. ...although a simple RegEx might do).  This filter prevents compliant strings from getting implicitly cast, as well as booleans (e.g. true -> 1). Floats (e.g. 3.14159), exponentials (e.g. 2.048e3), negatives, & number-resolving expressions (e.g. Math.E) are supported.
-  values = values.filter(value => typeof(value) === 'number')
+  // Assumption: Since not all values in the array are known to be numbers & I don't have the time to be comprehensive, I took extra precaution in the face of ambiguous constraints to only accept numeric types (to avoid booleans implicitly casting to numbers & save me the time of enumerating tests for unpacking nested containers, sanitizing malicious strings, etc. ...although a simple RegEx might do).  This filter prevents compliant strings from getting implicitly cast, as well as booleans (e.g. true -> 1). Floats (e.g. 3.14159), exponentials (e.g. 2.048e3), negatives, & number-resolving expressions (e.g. Math.E) are supported. The only good-faith assumption I make is that I'll be passed a valid JavaScript Array.
+  numbers = values.filter(value => typeof(value) === 'number') // no semi-colons necessary, but you'll see me prefer them in all assessments
 
-  return [Math.min(...values), Math.max(...values)]
-  // While this optimizes for readability & developer time cost, I still managed to needlessly sacrifice time efficiency & nobody likes "silent failures" (I could at least detect them & leave a console.warn). I have a frontend & backend to do on top of other interviews so no testing suite today.
+  return [Math.min(...numbers), Math.max(...numbers)]
+  // While this optimizes for readability & developer time cost, I still managed to needlessly sacrifice time efficiency & nobody likes "silent failures" (I could at least detect them & leave a console.warn). I have a frontend & backend to do on top of other interviews so no testing suite today. I prefer writing technical docs to just dumping this dense just-in-time prose between lines of code here, but I'm not getting paid to do this assessment so it'll do
 }
 
+// one personal test: minMax([2,false,"12",Math.PI,1]) ➞ [1, 3.1415926...]
+
+// Just to flex:
+minMax = array => (nums => [Math.min(...nums), Math.max(...nums)])(array.filter(x => typeof(x) === 'number'));
 
 
 /*
@@ -38,9 +42,14 @@ function minMax(values) {
     ➞ [{ text: 'Kim', value: '1'}, { text: 'Sally', value: 2}, { text: 'John', value: 3}]
 */
 
+const sortObjects = (values, sortBy) => values.sort((a, b) => a[sortBy] > b[sortBy]);
+
+// When I'm on my best behavior for readability:
+/*
 const sortObjects = (values, sortBy) => {
-  values.sort((a, b) => a[sortBy] > b[sortBy]); // sort objects by "sortBy" field name (ascending)
+  values.sort((a, b) => a[sortBy] > b[sortBy]); // sort objects by "sortBy" field name (ascending order)
   return values;
 }
+*/
 
 // This prompt didn't forbid side-effects like mutating inputs. JavaScript depends on "comparison functions" instead of [more readable] "key functions" like most modern languages (https://youtu.be/OSGv2VnC0go?t=683) so fingers crossed for ES7. I used a more modern arrow function to demonstrate my familiarity with both eras (even though I technically made use of one in the first prompt). If you attempt to use my `minMax` to validate my `sortObjects`, this will naturally fail silently whenever non-number types like compatible strings are tested.
